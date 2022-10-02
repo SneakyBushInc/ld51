fb71612930cbf4ed511bb05ae2fdce7cebcec467
iarwain <iarwain@orx-project.org>
2022-10-02 17:21:02 -0400

    - Don't check damage for dead characters

--- src/Character.cpp
+++ src/Character.cpp
@@ -56,17 +56,20 @@ void Character::OnDelete()
 
 orxBOOL Character::OnCollide(ScrollObject *_poCollider, orxBODY_PART *_pstPart, orxBODY_PART *_pstColliderPart, const orxVECTOR &_rvPosition, const orxVECTOR &_rvNormal)
 {
-    _poCollider->PushConfigSection();
-    if(orxConfig_GetBool("SingleHit"))
+    if(!IsDead())
     {
-        SetHealth(GetHealth() - orxConfig_GetFloat("Damage"));
-        _poCollider->SetLifeTime(orxFLOAT_0);
+        _poCollider->PushConfigSection();
+        if(orxConfig_GetBool("SingleHit"))
+        {
+            SetHealth(GetHealth() - orxConfig_GetFloat("Damage"));
+            _poCollider->SetLifeTime(orxFLOAT_0);
+        }
+        else
+        {
+            fDamage += orxConfig_GetFloat("Damage");
+        }
+        _poCollider->PopConfigSection();
     }
-    else
-    {
-        fDamage += orxConfig_GetFloat("Damage");
-    }
-    _poCollider->PopConfigSection();
 
     return Object::OnCollide(_poCollider, _pstPart, _pstColliderPart, _rvPosition, _rvNormal);
 }
